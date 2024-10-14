8fb9ced6c2e8c5b9d82200ec0ae6b60118569790
Anthony <agamarraj@pucp.edu.pe>
2024-10-14 09:24:37 -0500

    cambios

--- src/components/WorkProcessOne.js
+++ src/components/WorkProcessOne.js
@@ -7,9 +7,9 @@ const WorkProcessOne = () => {
       <div className='work-process-area pd-top-120 pd-bottom-90'>
         <div className='container'>
           <div className='section-title text-center'>
-            <h6 className='sub-title'>OUR SIMPLE PROCESS</h6>
+            <h6 className='sub-title'>Nuestro simple proceso</h6>
             <h2 className='title'>
-              World <span>Best Step</span> Our It Process
+              Pasos <span>claves</span> en nuestro proceso de creación
             </h2>
           </div>
           <div className='row'>
@@ -19,11 +19,10 @@ const WorkProcessOne = () => {
                   <img src='assets/img/icon/9.svg' alt='img' />
                 </div>
                 <div className='details'>
-                  <p className='process-count'>Work 01</p>
-                  <h5 className='mb-3'>Software Research</h5>
+                  <p className='process-count'>Paso 01</p>
+                  <h5 className='mb-3'>Evaluación de la problemática</h5>
                   <p className='content'>
-                    Research ipsum dolor sit consec tetur sed diam in the
-                    aliquam tempor
+                  Analizamos en profundidad la situación actual del negocio y proponemos soluciones estratégicas y efectivas.
                   </p>
                 </div>
               </div>
@@ -34,11 +33,10 @@ const WorkProcessOne = () => {
                   <img src='assets/img/icon/10.svg' alt='img' />
                 </div>
                 <div className='details'>
-                  <p className='process-count'>Work 02</p>
-                  <h5 className='mb-3'>Create Programs</h5>
+                  <p className='process-count'>Paso 02</p>
+                  <h5 className='mb-3'>Investigación de tecnologías y modelamiento</h5>
                   <p className='content'>
-                    Create ipsum dolor sit consec tetur sed diam in the aliquam
-                    tempor
+                  Investigamos y seleccionamos las tecnologías más adecuadas para las soluciones planteadas, modelando una versión preliminar optimizada.
                   </p>
                 </div>
               </div>
@@ -49,11 +47,10 @@ const WorkProcessOne = () => {
                   <img src='assets/img/icon/11.svg' alt='img' />
                 </div>
                 <div className='details'>
-                  <p className='process-count'>Work 03</p>
-                  <h5 className='mb-3'>Develope Software</h5>
+                  <p className='process-count'>Paso 03</p>
+                  <h5 className='mb-3'>Desarrollo</h5>
                   <p className='content'>
-                    Develope ipsum dolor sit consec tetur sed diam in the
-                    aliquam tempor
+                  Desarrollamos una herramienta eficiente que aborda de manera efectiva la problemática identificada.
                   </p>
                 </div>
               </div>
@@ -64,11 +61,10 @@ const WorkProcessOne = () => {
                   <img src='assets/img/icon/12.svg' alt='img' />
                 </div>
                 <div className='details'>
-                  <p className='process-count'>Work 04</p>
-                  <h5 className='mb-3'>Shop software</h5>
+                  <p className='process-count'>Paso 04</p>
+                  <h5 className='mb-3'>Implementación</h5>
                   <p className='content'>
-                    Shop ipsum dolor sit consec tetur Malesuada sed diam in the
-                    aliquam tempor
+                  Implementamos la solución en el negocio, ofreciendo soporte continuo durante el proceso de despliegue.
                   </p>
                 </div>
               </div>
