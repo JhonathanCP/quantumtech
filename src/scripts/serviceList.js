389636ef88c3048a952ebd2c64ed6738232d854f
Anthony <agamarraj@pucp.edu.pe>
2024-10-30 10:46:38 -0500

    cambios

--- src/scripts/serviceList.js
+++ src/scripts/serviceList.js
@@ -14,22 +14,6 @@ let serviceList = [
     des: "Proporcionamos servicios web integrales para potenciar tu presencia en línea y optimizar la interacción con tus usuarios.",
     img: "assets/img/service-icon/2.png",
   },
-  {
-    title: "Content Marketing",
-    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
-    img: "assets/img/service-icon/2.png",
-  },
-  {
-    title: "Mobile App Development",
-    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
-    img: "assets/img/service-icon/1.png",
-  },
-
-  {
-    title: "E-commerce Solutions",
-    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
-    img: "assets/img/service-icon/3.png",
-  },
 ];
 
 export default serviceList;
