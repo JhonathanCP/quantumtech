de9a7ec192e8c23af83063a378dba81aabab38d9
JhonathanCP <centtijhonathan@gmail.com>
2024-11-18 10:28:36 -0500

    envio formulario

--- src/components/ContactMain.js
+++ src/components/ContactMain.js
@@ -1,166 +1,133 @@
-'use client'
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+'use client';
+import React, { useState } from 'react';
 import { toast, Toaster } from 'react-hot-toast';
-const ContactMain = () => {
-  const form = useRef();
+import nodemailer from 'nodemailer';
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-    // Please See Documentation for more information
-    emailjs
-      .sendForm(
-        '**********', //YOUR_SERVICE_ID
-        '**********', //YOUR_TEMPLATE_ID
-        form.current,
-        '**********', //YOUR_PUBLIC_KEY
-      )
-      .then(
-        (result) => {
-          if (result.text === 'OK') {
-            toast.success('¡Mensaje enviado satisfactoriamente!');
-            form.current[0].value = '';
-            form.current[1].value = '';
-            form.current[2].value = '';
-            form.current[3].value = '';
-          }
-        },
-        (error) => {
-          if (error.text !== 'OK') {
-            toast.success('¡Mensaje no enviado!');
-          }
-        },
-      );
+const ContactMain = () => {
+  const [formData, setFormData] = useState({
+    user_name: '',
+    user_email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    // Configurar el transporte de Nodemailer
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'admin@quantumtechpe.com', // Tu correo
+        pass: 'mjil kawh gclz mjlm', // Contraseña generada en Google (App Password)
+      },
+    });
+
+    // Opciones del correo
+    const mailOptions = {
+      from: 'admin@quantumtechpe.com',
+      to: 'admin@quantumtechpe.com', // Destinatario (puede ser el mismo correo)
+      subject: `Nuevo mensaje de: ${formData.user_name} - ${formData.subject}`,
+      text: `Correo: ${formData.user_email}\n\n${formData.message}`,
+    };
+
+    try {
+      // Enviar el correo
+      await transporter.sendMail(mailOptions);
+      toast.success('¡Mensaje enviado satisfactoriamente!');
+      setFormData({
+        user_name: '',
+        user_email: '',
+        subject: '',
+        message: '',
+      });
+    } catch (error) {
+      console.error('Error enviando correo:', error);
+      toast.error('¡No se pudo enviar el mensaje!');
+    }
+  };
+
   return (
     <>
-      {/* ================= Contact Main start =================*/}
       <Toaster position="bottom-center" reverseOrder={false} />
-      <>
-        {/* contact area start */}
-        <div className="contact-area pd-top-120 pd-bottom-120">
-          <div className="container">
-            <div className="contact-page-inner bg-gray">
-              <div className="section-title mb-4 pb-2">
-                <h2 className="title">Mándanos un mensaje</h2>
-                <p className="content mb-0">
-                Déjanos tus dudas y sugerencias en nuestra bandeja de entrada, y nos pondremos 
+      <div className="contact-area pd-top-120 pd-bottom-120">
+        <div className="container">
+          <div className="contact-page-inner bg-gray">
+            <div className="section-title mb-4 pb-2">
+              <h2 className="title">Mándanos un mensaje</h2>
+              <p className="content mb-0">
+                Déjanos tus dudas y sugerencias en nuestra bandeja de entrada, y nos pondremos
                 en contacto contigo lo antes posible.
-                </p>
-              </div>
-              <form ref={form} onSubmit={sendEmail}>
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="single-input-inner">
-                      <input
-                        id="name"
-                        name="user_name"
-                        type="text"
-                        placeholder="Ingresa tu noombre."
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-12">
-                    <div className="single-input-inner">
-                      <input
-                        id="email"
-                        name="user_email"
-                        type="email"
-                        placeholder="Ingresa tu email"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-12">
-                    <div className="single-input-inner">
-                      <input
-                        id="subject"
-                        name="subject"
-                        type="text"
-                        placeholder="Ingresa el asunto"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="col-12">
-                    <div className="single-input-inner">
-                      <textarea
-                        name="message"
-                        id="massage"
-                        cols="1"
-                        rows="5"
-                        placeholder="Ingresa tu mensaje"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="col-12 text-center">
-                    <button
-                      type="submit"
-                      className="btn btn-base border-radius-5"
-                    >
-                      Enviar mensaje
-                    </button>
+              </p>
+            </div>
+            <form onSubmit={sendEmail}>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="single-input-inner">
+                    <input
+                      id="name"
+                      name="user_name"
+                      type="text"
+                      placeholder="Ingresa tu nombre"
+                      value={formData.user_name}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
                 </div>
-              </form>
-            </div>
+                <div className="col-md-12">
+                  <div className="single-input-inner">
+                    <input
+                      id="email"
+                      name="user_email"
+                      type="email"
+                      placeholder="Ingresa tu email"
+                      value={formData.user_email}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <div className="single-input-inner">
+                    <input
+                      id="subject"
+                      name="subject"
+                      type="text"
+                      placeholder="Asunto"
+                      value={formData.subject}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <div className="single-input-inner">
+                    <textarea
+                      id="message"
+                      name="message"
+                      placeholder="Mensaje"
+                      value={formData.message}
+                      onChange={handleChange}
+                      required
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <button type="submit" className="btn btn-primary">
+                    Enviar Mensaje
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-        {/* contact area end */}
-        {/* contact list start */}
-        <div className="contact-page-list">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-4 col-md-6">
-                <div className="media single-contact-list">
-                  <div className="media-left">
-                    <img src="assets/img/icon/13.svg" alt="img" />
-                  </div>
-                  <div className="media-body">
-                    <h5>Contactanos</h5>
-                    <h6>+51955517903</h6>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6">
-                <div className="media single-contact-list">
-                  <div className="media-left">
-                    <img src="assets/img/icon/14.svg" alt="img" />
-                  </div>
-                  <div className="media-body">
-                    <h5>Nuestro email</h5>
-                    <h6>admin@quantumtechpe.com</h6>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6">
-                <div className="media single-contact-list">
-                  <div className="media-left">
-                    <img src="assets/img/icon/15.svg" alt="img" />
-                  </div>
-                  <div className="media-body">
-                    <h5>Ubicación</h5>
-                    <h6>Lima, Perú</h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* contact list start */}
-        {/* map start */}
-        <div className="contact-g-map">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d97511.04316711595!2d-77.0428!3d-12.0464!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2spe!4v1589109092857!5m2!1sen!2spe"
-            title="new title"
-          />
-        </div>
-        {/* map end */}
-      </>
-
-      {/* ================= Contact Main End =================*/}
+      </div>
     </>
   );
 };
